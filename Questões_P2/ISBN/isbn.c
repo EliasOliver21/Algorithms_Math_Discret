e18122898c05723bc60a82831034d0e849148cfa
Elias_Oliveira <eliasoliveiradapaz@hotmail.com>
2025-01-21 15:16:01 -0300

    add: algorithms for calculate digit verificator of the CPF and ISBN

---
+++ Questões_P2/ISBN/isbn.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// Função para calcular o dígito verificador do ISBN-10
+int calcula_digito_isbn(int isbn[9]) {
+    int soma = 0;
+
+    // Cálculo do dígito verificador
+    for (int i = 0; i < 9; i++) {
+        soma += isbn[i] * (10 - i);
+    }
+    int digito = 11 - (soma % 11);
+    if (digito == 10) return 'X'; // Caso especial
+    if (digito == 11) return 0;
+    return digito;
+}
+
+int main() {
+    int isbn[9];
+
+    printf("Digite os 9 primeiros dígitos do ISBN (separados por espaço):\n");
+    for (int i = 0; i < 9; i++) {
+        scanf("%d", &isbn[i]);
+    }
+
+    int digito_isbn = calcula_digito_isbn(isbn);
+
+    printf("Dígito verificador do ISBN: \n");
+
+    if (digito_isbn == 'X') {
+        printf("Caso Especial D= X\n");
+    } else {
+        printf("%d\n", digito_isbn);
+    }
+
+    return 0;
+}
